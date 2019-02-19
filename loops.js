da432b4197bd053ecea1b8d711dcd4eeb26bbcbe
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-02-19 14:03:57 +0100

    adding comments to the for and while loop

--- loops.js
+++ loops.js
@@ -8,12 +8,21 @@
 
  var john = ['john', 'Mark', 1990, 'student', 'single'];
 
+ // Using the for loop
  for (i = 0; i < john.length; i++) {
     console.log(john[i]);
  }
 
+  // Using the while loop
  var i = 0;
  while (i < john.length) {
      console.log(john[i]);
      i++;
+ }
+
+
+// Continue & Break Statement
+ var john = ['john', 'Mark', 1990, 'student', 'single'];
+ for (i = 0; i < john.length; i++) {
+    console.log(john[i]);
  }
