b077c02109aba5c66df0c76d91fc83a8f7179a43
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-02-19 13:55:37 +0100

    simple for and while loop

---
+++ loops.js
@@ -0,0 +1,19 @@
+/*****************************
+ * Loops & Iterations
+ */
+
+ for (i = 0; i < 10; i++) {
+    console.log(i);
+ }
+
+ var john = ['john', 'Mark', 1990, 'student', 'single'];
+
+ for (i = 0; i < john.length; i++) {
+    console.log(john[i]);
+ }
+
+ var i = 0;
+ while (i < john.length) {
+     console.log(john[i]);
+     i++;
+ }
